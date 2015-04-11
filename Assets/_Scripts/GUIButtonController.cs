aefd1cf2eaf04c7f281a9becd32c522783493c4c
Fanakartal - ATOM <fanakartal@gmail.com>
2015-04-11 15:19:56 +0300

    side panel ve oyun içi basit menü yapıldı.

--- Assets/_Scripts/GUIButtonController.cs
+++ Assets/_Scripts/GUIButtonController.cs
@@ -3,10 +3,23 @@ using System.Collections;
 
 public class GUIButtonController : MonoBehaviour {
 
+    public static bool panelTurned;
+    
     //public GameObject PlayGame;
     
     public void GetNextLevel(int nextScene)
     {
         Application.LoadLevel(nextScene);
+    }
+
+    public void Restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+        Debug.Log(Application.loadedLevel);
+    }
+
+    public void OpenIngameSettings()
+    {
+        
     }
 }
---
+++ Assets/_Scripts/PanelTurner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelTurner : MonoBehaviour {
+
+    public void Turner()
+    {
+        if (gameObject.transform.rotation.y == 0.0f)
+        {
+            gameObject.transform.Rotate(0, -90.0f, 0);
+            Time.timeScale = 1.0f;
+        }
+        else
+        {
+            gameObject.transform.Rotate(0, 90.0f, 0);
+            Time.timeScale = 0.0f;
+        }
+    }
+}
