baec8ea14a0caf75c7a7d8b30bd9dda4fd14778c
Fanakartal - ATOM <fanakartal@gmail.com>
2015-05-26 20:52:40 +0300

    sliding menu deneme

---
+++ Assets/SidePanel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SidePanel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Menu()
+    {
+        
+    }
+}
--- Assets/_SimpleGUIPackage/_Scripts/PanelTurner.cs
+++ Assets/_SimpleGUIPackage/_Scripts/PanelTurner.cs
@@ -3,7 +3,17 @@ using System.Collections;
 
 public class PanelTurner : MonoBehaviour {
 
-    public void Turner()
+    private Animator anim;
+
+    private bool isPaused = false;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.Play("Idle");
+    }
+    
+    /*public void Turner()
     {
         if (gameObject.transform.rotation.y == 0.0f)
         {
@@ -15,5 +25,26 @@ public class PanelTurner : MonoBehaviour {
             gameObject.transform.Rotate(0, 90.0f, 0);
             Time.timeScale = 0.0f;
         }
+    }
+     */
+
+    public void PauseGame()
+    {
+        //anim.enabled = false;
+
+        if (anim.GetBool("isIdle") == true)
+        {
+            if (anim.GetBool("isIn") == false)
+            {
+                //anim.Play("SlideIn");
+                //anim.SetBool("isIn", true);
+                Time.timeScale = 0;
+            }
+        }
+        /*    anim.enabled = true;
+            isPaused = false;
+            anim.Play("SlideOut");
+            Time.timeScale = 1;
+         * */
     }
 }
