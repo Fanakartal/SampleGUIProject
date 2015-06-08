66e45789fe5ff7687690ed4d3bda87bf14d4dd21
Fanakartal - ATOM <fanakartal@gmail.com>
2015-06-08 19:16:23 +0300

    döner yan menü

--- Assets/_SimpleGUIPackage/_Scripts/GUIButtonController.cs
+++ Assets/_SimpleGUIPackage/_Scripts/GUIButtonController.cs
@@ -3,7 +3,17 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class GUIButtonController : MonoBehaviour 
-{    
+{
+    public Animator contentPanel;
+    public Animator gearImage;
+
+    void Start()
+    {
+        RectTransform transform = contentPanel.gameObject.transform as RectTransform;
+        Vector2 position = transform.anchoredPosition;
+        position.y -= transform.rect.height;
+        transform.anchoredPosition = position;
+    }
     
     public void GetNextLevel(int nextScene)
     {
@@ -13,5 +23,18 @@ public class GUIButtonController : MonoBehaviour
     public void Restart()
     {
         Application.LoadLevel(Application.loadedLevel);
+    }
+
+    public void ToggleSettings()
+    {
+        contentPanel.enabled = true;
+
+        bool isHidden = contentPanel.GetBool("isHidden");
+        contentPanel.SetBool("isHidden", !isHidden);
+
+        gearImage.enabled = true;
+
+        bool isHidden2 = gearImage.GetBool("isHidden2");
+        gearImage.SetBool("isHidden2", !isHidden2);
     }
 }
